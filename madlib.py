3946ad8db26d95b5ca1ec72aa56e768dc51266d5
michellemontevago <42847071+michellemontevago@users.noreply.github.com>
2018-09-13 15:04:52 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,6 @@
+w = input ("Enter a noun: ")
+x = input ("Enter a verb: ")
+y = input (" Enter an adjective: ")
+z = input ("Enter a place: ")
+
+print ("Can we take the " + y + " " + w + " to go " + x + " at the " + z + "?")
