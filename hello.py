e6fa34e627a67c0f233004e0b6059ccaa94f8ac6
michellemontevago <42847071+michellemontevago@users.noreply.github.com>
2018-09-13 14:28:59 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,16 @@
+#Author: Michelle Montevago
+# September 13th, 2018 
+
+
+#fucntion declaration (or head) 
+def main():
+    # everything below that is indented is the function body 
+    name = input ("What is your name?")
+    #call the print function 
+    print("Hello, " + name + ".")
+
+main ()
+          
+# there is a temoorary value created
+# this is the text that the user types
+y = int (input("Enter an integer: ") ) 
---
+++ rover.py
@@ -0,0 +1,11 @@
+# Author: Michelle Montevago
+
+# make a variable named 'speedOfLight'
+# we also say that 'speedOfLight' is an identifier for this variable
+
+speedOfLight = 186000
+distance = 34000000
+
+timeToSend = distance / speedOfLight
+
+print("It will take", timeToSend, "seconds to send the photo back to Earth.")
