945dad7f06017a8668169779777c12c26579a2a9
Michelle Montevago <michelle.montevago1@marist.edu>
2018-09-18 14:22:05 -0400

    added main function to madlib

--- madlib.py
+++ madlib.py
@@ -1,6 +1,9 @@
-w = input ("Enter a noun: ")
-x = input ("Enter a verb: ")
-y = input (" Enter an adjective: ")
-z = input ("Enter a place: ")
+def main ():
+    w= input ("Enter a noun: ")
+    x = input ("Enter a verb: ")
+    y = input (" Enter an adjective: ")
+    z = input ("Enter a place: ")
 
-print ("Can we take the " + y + " " + w + " to go " + x + " at the " + z + "?")
+    print ("Can we take the " + y + " " + w + " to go " + x + " at the " + z + "?")
+
+main ()
